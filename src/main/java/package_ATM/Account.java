ec635d3b616371947ba3262b18ba4046346b529c
NajulaMN <lenovovibe9656@gmail.com>
2023-01-31 21:29:23 +0530

    Project is done.

--- src/main/java/package_ATM/Account.java
+++ src/main/java/package_ATM/Account.java
@@ -9,6 +9,7 @@ public class Account {
 	private int pinNumber;
 	private double checkingBalance;
 	private double savingBalance;
+	
 	Scanner sc=new Scanner(System.in);
 	DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
 	
@@ -62,9 +63,60 @@ public class Account {
 	
 	
 	public void getCheckingWithdrawInput() {
-		System.out.println("Checking Account Balance");
+		System.out.println("Checking Account Balance: "+moneyFormat.format(checkingBalance));
+		System.out.println("Amount you want to withdraw from Checking Account: ");
+		double amount=sc.nextDouble();
+		
+		if ((checkingBalance-amount) >= 0) {
+			calcCheckingWithdraw(amount);
+			System.out.println("New Checking Account Balance: "+moneyFormat.format(checkingBalance));
+		}
+		else {
+			System.out.println("Balance cannot be Negative.");
+		}
 	}
 	
+	public void getSavingWithdrawInput() {
+		System.out.println("Saving Account Balance: "+moneyFormat.format(savingBalance));
+		System.out.println("Amount you want to withdraw from Saving Account: ");
+		double amount=sc.nextDouble();
+		
+		if ((savingBalance-amount) >= 0) {
+			calcSavingWithdraw(amount);
+			System.out.println("New saving Account Balance: "+moneyFormat.format(savingBalance));
+		}
+		else {
+			System.out.println("Balance cannot be Negative.");
+		}
+	}
 	
+	public void getCheckingDepositInput() {
+		System.out.println("Checking Account Balance: "+moneyFormat.format(checkingBalance));
+		System.out.println("Amount you want to deposit to Checking Account: ");
+		double amount=sc.nextDouble();
 
-}
+	if ((checkingBalance+amount) >= 0) {
+		calcCheckingDeposit(amount);
+		System.out.println("New Checking Account Balance: "+moneyFormat.format(checkingBalance));
+	}
+	else {
+		System.out.println("Balance cannot be Negative.");
+	}
+	}
+	
+	public void getSavingDepositInput() {
+		System.out.println("Saving Account Balance: "+moneyFormat.format(savingBalance));
+		System.out.println("Amount you want to deposit to Saving Account: ");
+		double amount=sc.nextDouble();
+		
+		if ((savingBalance+amount) >= 0) {
+			calcSavingWithdraw(amount);
+			System.out.println("New saving Account Balance: "+moneyFormat.format(savingBalance));
+		}
+		else {
+			System.out.println("Balance cannot be Negative.");
+		}
+	}
+	}
+
+
--- src/main/java/package_ATM/OptionMenu.java
+++ src/main/java/package_ATM/OptionMenu.java
@@ -1,5 +1,98 @@
 package package_ATM;
 
-public class OptionMenu {
+import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.HashMap;
+import java.util.Scanner;
 
-}
+public class OptionMenu extends Account {
+	Scanner menuInput=new Scanner(System.in);
+	DecimalFormat moneyFormat = new DecimalFormat("'$'###,##0.00");
+	
+	HashMap<Integer,Integer> data=new HashMap<Integer,Integer>();
+
+	public void getLogin() throws IOException {
+		// TODO Auto-generated method stub
+		int x=1;
+		
+		do {
+			try {
+				data.put(994701,2255); //(AccountNumber,PinNumber)
+				data.put(807586,3214);
+				System.out.println("Welcome to the ATM Project!");
+				System.out.println("Enter your Account Number: ");
+				setAccountNumber(menuInput.nextInt());
+				
+				System.out.println("Enter your PIN Number: ");
+				setPinNumber(menuInput.nextInt());
+			}
+			catch (Exception e) {
+				System.out.println("Invalid Character(s). Only Numbers.");
+				x=2;
+			}
+			
+			/* 
+			 * for(Map.Entry<Integer , Integer> it:data.entrySet()) {
+			 * if(it.getKey()==getAccountNumber() && it.getValue()==getPinNumber) {
+			 * getAccountType();
+			 * }
+			 * }
+			 * 
+			 */
+			
+			int an=getAccountNumber();
+			int pn=getPinNumber();
+			
+			if (data.containsKey(an) && data.get(an) == pn) {
+				getAccountType();
+			}
+			else {
+				System.out.println("Wrong Customer Number or Pin Number");
+			}
+		}
+		while (x==1);
+		}
+
+	private void getAccountType() {
+		// TODO Auto-generated method stub
+		System.out.println("Select the Account Type you want to access:");
+		System.out.println("Type 1-Checking Account");
+		System.out.println("Type 2-Saving Account");
+		System.out.println("Type 3-Exit");
+		
+		int selection=menuInput.nextInt();
+		
+		switch(selection) {
+		case 1:
+			getChecking();
+			break;
+			
+		case 2:
+			getSaving();
+			break;
+			
+		case 3:
+			System.out.println("Thank you for using this ATM, Have a Nice Day.");
+			break;
+			
+		default:
+			System.out.println("Invalid Input");
+		}
+		
+		
+		
+	}
+
+	private void getSaving() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void getChecking() {
+		// TODO Auto-generated method stub
+		
+	}
+		
+	}
+
+
